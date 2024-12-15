ec07d9eb95251d9cd4fc34850105461263db9e4a
Jaeho Yi <akghxhs55@gmail.com>
2024-12-15 16:41:18 +0900

    Add: add alignment for uniform buffer object

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -2,7 +2,7 @@
 #pragma shader_stage(vertex)
 
 
-layout(binding = 0) uniform UniformBufferObject {
+layout(set = 0, binding = 0) uniform UniformBufferObject {
     mat4 model;
     mat4 view;
     mat4 proj;
